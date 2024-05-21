31a6786315d2e92186c444c5d226da3fc1f239f0
danieloliveiravaz1 <danieloliveiravaz1@gmail.com>
2024-05-21 17:15:27 +0000

    Atualização da font family de todos os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,23 @@ import Home from './Pages/Home.jsx'
 import Projeto from './Pages/Projeto.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+/*<style>
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style> */
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path='/Projeto' Component={Projeto} />
-        <Route path='/ConnectWorkbench' Component={ConnectWorkbench}/>
-      </Routes>
-    </BrowserRouter>
+    <div style={{fontFamily: 'Fira Sans'}}>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+      </style>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path='/Projeto' Component={Projeto} />
+          <Route path='/ConnectWorkbench' Component={ConnectWorkbench}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
